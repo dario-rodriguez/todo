2c97a8af2f78ee1e3e76d1df3cd985bd0d159e9b
Dario Rodriguez Gonzalez <dario.rodriguez-gonzalez@capgemini.com>
2020-04-15 14:26:27 +0200

    Added columns to todo.entity

--- src/app/todo/model/entities/todo.entity.ts
+++ src/app/todo/model/entities/todo.entity.ts
@@ -1,5 +1,22 @@
-import { Entity } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { BaseEntity } from '../../../shared/model/entities/base-entity.entity';
+import { User } from '../../../core/user/model/entities/user.entity';
 
 @Entity()
-export class Todo extends BaseEntity {}
+export class Todo extends BaseEntity {
+  @Column('varchar', { length: 255, nullable: false })
+  description!: string;
+  @Column('tinyint', { nullable: false, default: 1 })
+  priority!: number;
+  @Column('boolean', { nullable: false, default: false })
+  completed!: boolean;
+  @Column('bigint', { nullable: false })
+  userId!: number;
+
+  @ManyToOne(
+    () => User,
+    user => user.id,
+  )
+  @JoinColumn()
+  user?: User;
+}
