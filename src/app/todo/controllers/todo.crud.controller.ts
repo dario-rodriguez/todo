c5d3dc3aaceb7f8d5e7202017e13bbc86aa3cdd0
Dario Rodriguez Gonzalez <dario.rodriguez-gonzalez@capgemini.com>
2020-04-15 14:49:43 +0200

    Secured all endpoints

--- src/app/todo/controllers/todo.crud.controller.ts
+++ src/app/todo/controllers/todo.crud.controller.ts
@@ -1,9 +1,12 @@
-import { Controller } from '@nestjs/common';
-import { Crud } from '@nestjsx/crud';
+import { Controller, UseGuards, BadRequestException } from '@nestjs/common';
+import { Crud, CrudAuth, Override, CrudController, ParsedRequest, CrudRequest } from '@nestjsx/crud';
 import { CrudType } from '@devon4node/common/serializer';
 import { Todo } from '../model/entities/todo.entity';
 import { TodoCrudService } from '../services/todo.crud.service';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '../../core/user/model/entities/user.entity';
+import { GetUser } from '../../core/auth/decorators/get-user.decorator';
 
 @Crud({
   model: {
@@ -14,6 +17,29 @@ import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 @Controller('todo/todos')
 @ApiTags('todo')
 @ApiBearerAuth()
+@UseGuards(AuthGuard())
+@CrudAuth({
+  property: 'user',
+  filter: (user: User) => ({
+    userId: user.id,
+  }),
+  persist: (user: User) => ({
+    userId: user.id,
+  }),
+})
 export class TodoCrudController {
   constructor(public service: TodoCrudService) {}
+
+  get base(): CrudController<Todo> {
+    return this;
+  }
+
+  @Override()
+  deleteOne(@GetUser() user: User, @ParsedRequest() req: CrudRequest): Promise<void | Todo> {
+    if (user.id !== req.parsed.paramsFilter[0].value) {
+      throw new BadRequestException();
+    }
+
+    return this.base.deleteOneBase!(req);
+  }
 }
